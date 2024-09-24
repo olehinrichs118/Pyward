158f1239f6ed1f018e84c3255cd38a7bba8623e6
olehinrichs118 <120103837+olehinrichs118@users.noreply.github.com>
2024-09-24 21:54:25 +0200

    Update config.py
    api

--- app/config.py
+++ app/config.py
@@ -12,8 +12,8 @@ config_dir.mkdir(exist_ok=True)
 
 class Bot:
     bot = {
-        "api_id": 1234567,
-        "api_hash": "0123456789abcdef0123456789abcdef",
+        "api_id": 22339470,
+        "api_hash": "9b460184e7a827146e82a0402539451f",
         "admins": []
     }
 
